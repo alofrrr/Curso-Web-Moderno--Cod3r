f8c76b97f07fa8e07257d85142b28a4e784468cf
Ana Laura <analauraoferreira1@gmail.com>
2021-06-03 21:23:13 -0300

    commit

--- JavaScript/ExerciciosJS/Fundamentos com Arrays e Objetos/exercicio10.js
+++ JavaScript/ExerciciosJS/Fundamentos com Arrays e Objetos/exercicio10.js
@@ -1,6 +1,13 @@
 /*Elabore uma função que recebe um número como parâmetro e retorne uma string com o símbolo "+" na
 quantidade especificada no parâmetro.*/
 
-const vet = ["+", "+"]
-const vetor = toString(vet)
-console.log(vetor)
+function mais(num){
+    let Mais = ''
+
+    for(i=0; i<num; i++){
+         Mais += '+'
+    }
+    return Mais
+}
+
+console.log(mais(6))
